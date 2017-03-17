cae7853e01eec336f626b968fa58d7871e6e40f2
Nicholas <nicholas.allio@me.com>
2017-03-17 12:21:13 +0100

    Initial commit

---
+++ NexmoTest/NexmoTest.h
@@ -0,0 +1,19 @@
+//
+//  NexmoTest.h
+//  NexmoTest
+//
+//  Created by Nicholas Allio on 17/03/2017.
+//  Copyright © 2017 Nicholas Allio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NexmoTest.
+FOUNDATION_EXPORT double NexmoTestVersionNumber;
+
+//! Project version string for NexmoTest.
+FOUNDATION_EXPORT const unsigned char NexmoTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NexmoTest/PublicHeader.h>
+
+
